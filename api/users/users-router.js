6ba2a46da526198755a532e4fbc799dd8853e36d
KRQuinn <kr.quinn82@gmail.com>
2021-11-16 01:02:13 -0800

    Ryan Quinn/ Authentication not posting as intended, will revisit tomorrow morning

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 const restricted = require("../auth/restricted-middleware.js");
-const only = require('../auth/check-role-middleware')
+const only = require('../auth/check-roles-middleware')
 
 router.get("/", restricted, only('admin'), (req, res, next) => {
   Users.find()
--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
       client: "sqlite3",
       useNullAsDefault: true,
       connection: {
-        filename: "./database/potluck.db3",
+        filename: "./data/potluck.db3",
       },
       pool: {
         afterCreate: (conn, done) => {
